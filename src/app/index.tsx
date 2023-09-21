99e136e63ce1b32c3d32b8094591593ef69bdaf2
Kevin Hoang <knhoangre@gmail.com>
2023-09-21 14:55:36 -0400

    Removed index.tsx (Was used for Express & Babel)

--- src/app/index.tsx
+++
@@ -1,7 +0,0 @@
-import { App } from './page';
-import { createRoot } from 'react-dom/client';
-// import React from 'react';
-
-const node = document.getElementById('app');
-const root = createRoot(node);
-root.render(<App />);
