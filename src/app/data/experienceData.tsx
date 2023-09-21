0441ff6cff81a7663cd6f672dd7720c28a38108a
Kevin Hoang <knhoangre@gmail.com>
2023-09-21 14:13:54 -0400

    Create a data file to add in all of my experience and projects instead of creating multiple elements. May want to add a database in the future.

---
+++ src/app/data/experienceData.tsx
@@ -0,0 +1,23 @@
+export const experienceData = [
+  {
+    id: 1,
+    time: 'Jun 2021 - Present',
+    position: 'Real Estate Agent',
+    name: 'eXp Realty & Keller Williams Realty',
+    text: 'Designed and analyzed complex system functionalities to significantly increase efficiency. Also, managed and communicated with a team of engineers to certify processes.'
+  },
+  {
+    id: 2,
+    time: 'Jun 2021 - Present',
+    position: 'Mechanical Design Engineer',
+    name: 'Collins Aerospace',
+    text: 'Designed and analyzed complex system functionalities to significantly increase efficiency. Also, managed and communicated with a team of engineers to certify processes.'
+  },
+  {
+    id: 3,
+    time: 'Jun 2017 - Dec 2019',
+    position: 'Aerospace System Engineer',
+    name: 'Belcan',
+    text: 'Performed software testing through different builds to ensure that engine performance has increased and other sections have not been compromised.'
+  },
+];
---
+++ src/app/data/projectData.tsx
@@ -0,0 +1,47 @@
+export const projectData = [
+  {
+    id: 1,
+    time: '2023',
+    position: 'Full Stack Engineer',
+    name: 'Listing Platform',
+    link: false,
+    redirectLink: '',
+    text: 'Real estate marketplace that provides users with resources about properties on the market. It lists all the properties on the market and gives the users power to make an informed decision based on user inputs.'
+  },
+  {
+    id: 2,
+    time: '2023',
+    position: 'Full Stack Engineer',
+    name: 'Shelf Share',
+    link: true,
+    redirectLink: 'shelfshare.org',
+    text: 'Book-sharing platform where users can rent out their personal libraries. Users can sign up and log in via Google accounts, input essential information, scan their books into the system, and manage borrowing and lending activities. The platform also offers a book review feature and an API for advertisers to analyze book trends.'
+  },
+  {
+    id: 3,
+    time: '2023',
+    position: 'Back-End Engineer',
+    name: 'Atelier Back-End Design',
+    link: false,
+    redirectLink: '',
+    text: 'E-commerce store showcasing capability of handling high traffic volumes and performance. Custom routes were created to match with the Atelier User Interface Design project. Routes were tested for average response times and success rates.'
+  },
+  {
+    id: 4,
+    time: '2023',
+    position: 'Front-End Engineer',
+    name: 'Atelier User Interface Design',
+    link: false,
+    redirectLink: '',
+    text: 'E-commerce store showcasing a variety of clothing lines with detailed information about each product. Every product has its own different styles, description, related items, question and answers, and review sections.'
+  },
+  {
+    id: 5,
+    time: '2022',
+    position: 'Software Engineer',
+    name: 'Facebook Marketplace Postings',
+    link: false,
+    redirectLink: '',
+    text: 'Real estate marketplace that provides users with resources about properties on the market. It lists all the properties on the market and gives the users power to make an informed decision based on user inputs.'
+  },
+];
