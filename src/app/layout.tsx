6533ea352b60494e315c5b58dfbb119da59891c0
Kevin Hoang <knhoangre@gmail.com>
2023-10-10 17:49:21 -0400

    Changed title of the website.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>Kevin Hoang</title>
+        <title>Kevin Hoang Portfolio</title>
         <link rel='icon' href='/favicon.ico' />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
